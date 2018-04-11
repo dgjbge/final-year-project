e87aa6e205fa07b0235ddfa18d65bb20c0c281a8
Vladislav Romanov <vr237@exeter.ac.uk>
2018-04-11 11:25:36 +0100

    Added real-time data functionality

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ window.onload = function(){
 
     const fileUploader = document.querySelector('px-file-upload');
     fileUploader.addEventListener('px-file-upload-files-changed', function() {
-        console.log('file!!');
         validateFile()
     });
     modal.addEventListener('btnModalPositiveClicked', function() {
@@ -17,6 +16,8 @@ window.onload = function(){
 };
 
 let styleCache = {};
+let vectorLayer;
+
 const styleFunction = function(feature) {
     let featureType = feature.get('type');
 
@@ -105,41 +106,6 @@ function uploadFile() {
 
 }
 
-//popup code
-
-/*
-let popup = document.getElementById('popup');
-let popupInfo = document.getElementById('popup-info');
-let popupCloser = document.getElementById('closer');
-
-let overlay = new ol.Overlay({
-    element: popup,
-    autoPan: true,
-    autoPanAnimation: {
-        duration: 200
-    }
-});
-*/
-
-/*popupCloser.onclick = function() {
-    overlay.setPosition(undefined);
-    return false;
-};*/
-
-/*const map = new ol.Map({
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        }),
-    ],
-    overlays: [overlay],
-    target: 'map',
-    view: new ol.View({
-        center: ol.proj.transform([-3.5, 50.74], 'EPSG:4326', 'EPSG:3857'),
-        zoom: 13
-    })
-});*/
-
 function createDataSource() {
     let vectorSource = new ol.source.Vector({
         features: (new ol.format.GeoJSON()).readFeatures(geojsonObject, {
@@ -147,7 +113,7 @@ function createDataSource() {
         })
     });
 
-    const vectorLayer = new ol.layer.Vector({
+    vectorLayer = new ol.layer.Vector({
         source: vectorSource,
         style: styleFunction
     });
@@ -188,15 +154,75 @@ function generateInfo(vectorLayer) {
 
 }
 
-/*
-map.on('click', function(e) {
-    let feature = map.forEachFeatureAtPixel(e.pixel, function(feat) {return feat;});
+function realTimeDataUpload() {
+    const modal = document.getElementById('real-time-data-modal');
+    modal.modalButtonClicked();
+    modal.addEventListener('btnModalPositiveClicked', function() {
+        uploadRealTimeData();
+    });
+}
+let realTimeData;
 
-    if(feature) {
-        console.log(popupInfo);
-        console.log(document.getElementById('popup-info'));
-        document.getElementById('popup-info').innerHTML = '<p>Feature Type:</p>' + feature.get('type');
-        console.log(overlay);
-        overlay.setPosition(e.coordinate);
+function uploadRealTimeData() {
+    const file = document.querySelectorAll('px-file-upload')[1].files[0];
+    const reader = new FileReader();
+    let realTimeContents;
+
+    reader.onload = function(e) {
+        realTimeContents = e.target.result;
+        realTimeData = JSON.parse(realTimeContents);
+        linkRealTimeData(realTimeData);
+    };
+
+    reader.readAsText(file, 'UTF-8');
+
+}
+
+function linkRealTimeData(data) {
+    let features = geojsonObject.features;
+    let realTimeFeatures = data.data;
+    features.forEach(function(feature) {
+        let featureID = feature.properties.id;
+        if(realTimeFeatures[featureID] !== undefined) {
+            let realTimeReadings = realTimeFeatures[featureID];
+            feature.properties['realTimeData'] = realTimeReadings['readings']
+        }
+    });
+    console.log(vectorLayer.getSource().getFeatures());
+}
+
+const overloadedCableStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+        color: 'orange',
+        width: 2
+    })
+});
+
+const normalCableStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+        color: 'green',
+        width: 2
+    })
+});
+
+const underloadedCableStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+        color: 'blue',
+        width: 2
+    })
+});
+
+
+/*function playData() {
+    const networkFeatures = vectorLayer.getSource().getFeatures();
+    for(let i = 0; i < 24; i++){
+        networkFeatures.forEach(function(feature) {
+            if(feature.properties.type === 'underground_cable' && feature.properties.realTimeData) {
+                const reading = feature.properties.realTimeData[i];
+                if
+            }
+        })
     }
-});*/
+}*/
+
+console.log(vectorLayer.getSource().getFeatures());
